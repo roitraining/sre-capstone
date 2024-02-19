64bcacee2c95b4e95e38b30892183b04f4fa3988
Steve Lockwood <stevelockwood@users.noreply.github.com>
2024-02-18 22:49:28 -0500

    Update gke-cluster.tf - added node sa

--- gke-cluster.tf
+++ gke-cluster.tf
@@ -6,6 +6,7 @@ resource "google_container_cluster" "gke-cluster" {
 
   node_config {
     machine_type = "e2-small"
+    service_account = "${var.gke-node-service-account-name}@${var.project_id}.iam.gserviceaccount.com"
     spot = true
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
